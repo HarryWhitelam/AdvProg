9d5638771c270775cc077cbdb229ba928578b050
irie612 <57913132+irie612@users.noreply.github.com>
2023-01-02 15:07:23 +0000

    get rid of uncessary code

--- Backend/Interpreter.fs
+++ Backend/Interpreter.fs
@@ -1,8 +1,6 @@
 ﻿namespace Backend
 
 module Interpreter =
-    open System
-
     let interpret expression = 
         if Seq.length expression = 0 then null
         else 
@@ -12,10 +10,3 @@ module Interpreter =
             else null
 
     let updateVarStore = Executor.variableStore
-
-    let rec printTList (lst:list<Token>) : list<string> = 
-        match lst with
-        head::tail ->   Console.Write("{0} ",head.ToString())
-                        printTList tail
-        | [] ->         Console.Write("EOL\n")
-                        []
